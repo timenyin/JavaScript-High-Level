1783d60dc849150bea284fcebedd3ba09b28f095
Harmony <47876447+timenyin@users.noreply.github.com>
2023-12-11 14:17:05 +0100

    still progress
    in THIS KEYWORDS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* === scoping =======
+
 const firstName = "jonas";
 calcAge(1991);
 
@@ -31,3 +33,59 @@ function calcAge(birthYear) {
   printAge();
   return age;
 }
+
+*/
+
+// Hosting and TDZ ===
+/*
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = "jonas";
+let job = "teacher";
+const year = 1889;
+
+// for Functions ===
+console.log(addDecl(2, 4));
+//console.log(addExpr(8, 4));   // are in the TDZ
+//console.log(addArrow(5, 4)); // are in the TDZ
+console.log(addArrows);
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+var addArrows = (a, b) => a + b;
+*/
+
+// Example
+// // console.log(undefined);
+// if (!sumProduct) deleteshoppingCart();
+// var sumProduct = 18;
+// function deleteshoppingCart() {
+//   console.log("All product deleted");
+// }
+// Example 2
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+
+// ======The THIS KEYWORD =======
+console.log(this);
+
+const calage = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calage(1991);
