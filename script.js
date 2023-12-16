c102b7c6727bdf4ad6ad78b447efc29492cef340
Harmony <47876447+timenyin@users.noreply.github.com>
2023-12-16 23:53:05 +0100

    PRIMITIVES VS OBJECTS  VIDEO 99
    PRIMITIVES VS OBJECTS (PRIMITIVE VS REFERENCE TYPES

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ var addArrows = (a, b) => a + b;
 // console.log(y === window.y);
 
 // ======The "THIS" KEYWORD =======
+/*
 console.log(this);
 
 const calAge = function (birthYear) {
@@ -89,3 +90,80 @@ const calAge = function (birthYear) {
 };
 
 calAge(1991);
+
+const calAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this); // this will not work because the arrow function use the lexical function
+};
+
+calAgeArrow(1991);
+
+const harmony = {
+  // this  the object
+  year: 1993,
+  calcAge: function () {
+    // this the method
+    console.log(2024 - this.year);
+    console.log(this);
+  },
+};
+harmony.calcAge();
+
+const precious = {
+  year: 1995,
+};
+
+precious.calcAge = harmony.calcAge; // this called method browning
+precious.calcAge();
+
+const f = harmony.calcAge;
+
+
+// ====================================
+const harmony = {
+  // this  the object
+  firstName: "Harmony",
+  year: 1993,
+  calcAge: function () {
+    // this the method
+    console.log(2024 - this.year);
+    //console.log(this);
+
+    const self = this;
+    // solution 2 that nee extra variable
+    // const isMillennial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    const isMillennial = () => {
+      // arrow function inherits
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillennial();
+  },
+  greet: () => console.log(`hey ${this.firstName}`), // won't work because is a arrow function is been used
+};
+
+harmony.greet();
+harmony.calcAge();
+*/
+
+// Arguments "THIS keywords"
+
+var addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 6, 9, 10);
+var addArrow = (a, b) => {
+  //console.log(arguments); not that important in javascript
+  return a + b;
+};
+
+addArrow(2, 3, 5);
+
+// PRIMITIVES VS OBJECTS (PRIMITIVE VS REFERENCE TYPES)
